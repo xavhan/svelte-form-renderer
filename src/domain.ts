e2c0a8d2720b9dde4c5f302b8243b03d5937637a
Xavier Haniquaut <xavier.haniquaut@gmail.com>
2021-07-15 13:20:55 +0200

    fix

--- src/domain.ts
+++ src/domain.ts
@@ -1,12 +1,12 @@
-export const isStringField = (field: Field): field is StringField => {
+export const isStringField = (field: AnyField): field is StringField => {
   return field.type === "string";
 };
-export const isBooleanField = (field: Field): field is BooleanField => {
+export const isBooleanField = (field: AnyField): field is BooleanField => {
   return field.type === "boolean";
 };
 export const isChoice = (field: StringField): field is ChoiceField => {
   return "choices" in field;
 };
-export const isMultiStringField = (field: Field): field is MultiStringField => {
+export const isMultiStringField = (field: AnyField): field is MultiStringField => {
   return isStringField(field) && "multi" in field;
 };
